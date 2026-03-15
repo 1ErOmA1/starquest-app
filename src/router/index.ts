877ab3d6451afe5b9081517a7eaa467a016eab88
1ErOmA1 <100420970+1ErOmA1@users.noreply.github.com>
2026-03-15 18:12:33 +0300

    fix: redirect to home on unknown route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,17 @@
+// import { createRouter, createWebHashHistory } from "vue-router";
+// import HomeView from "../views/HomeView.vue";
+
+// const router = createRouter({
+//   history: createWebHashHistory(),
+//   routes: [
+//     { path: "/", component: HomeView },
+//     { path: "/tasks", component: () => import("../views/TasksView.vue") },
+//     { path: "/shop", component: () => import("../views/ShopView.vue") },
+//   ],
+// });
+
+// export default router;
+
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
@@ -7,6 +21,7 @@ const router = createRouter({
     { path: "/", component: HomeView },
     { path: "/tasks", component: () => import("../views/TasksView.vue") },
     { path: "/shop", component: () => import("../views/ShopView.vue") },
+    { path: "/:pathMatch(.*)*", redirect: "/" },
   ],
 });
 
